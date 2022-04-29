bbf870466c14080fd4c2a9446ea7e1793700a9d5
Kassin97 <romankomarnytsky@gmail.com>
2022-04-30 00:39:21 +0300

    add logs

--- src/ajax.php
+++ src/ajax.php
@@ -2,6 +2,7 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 if (!empty($_REQUEST)) {
+    Log::add('Запит від '.$_SERVER['REMOTE_ADDR'].': '.$_REQUEST['link']);
     $arResult = (new Controller())->init($_REQUEST);
     echo json_encode($arResult);
 }
